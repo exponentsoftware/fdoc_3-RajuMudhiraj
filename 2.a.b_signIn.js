687ab305983304c7dacbdf6d91c95b8d8e1c8ba5
RajuMudhiraj <rajuthecodingcreature@gmail.com>
2022-01-17 21:26:13 +0530

    Solved ninth problem

---
+++ 2.a.b_signIn.js
@@ -0,0 +1,27 @@
+// Hypothetically dealing with mongodb via mongoose library
+users.find({ email: req.body.email })
+    .exec()
+    .then(user => {
+        if (user.length >= 1) {
+            if (user.password === req.body.password) {
+                res.status(200).json({ message: "User logged in successfully!" })
+            }
+            else {
+                res.status(201).json({
+                    message: 'Wrong credentials'
+                })
+            }
+        }
+        else {
+            res.status(201).json({
+                message: 'User not registered'
+            })
+        }
+
+
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    });
